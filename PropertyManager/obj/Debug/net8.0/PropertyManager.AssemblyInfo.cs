abb17135faf4fa3ec2d01d13afb67f0c86cf124e
William Villa <williamvilla727@gmail.com>
2024-11-23 12:36:19 -0500

    Actualización

--- PropertyManager/obj/Debug/net8.0/PropertyManager.AssemblyInfo.cs
+++ PropertyManager/obj/Debug/net8.0/PropertyManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PropertyManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29dcfb91e4e578ce24bc4037e01a70826b388c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5d095e7b64513b751d4e5e7fb34661064ab375b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PropertyManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PropertyManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
